b3c859ebaf7fa73ff0b01d44be30614e146a9661
Khushi Patel <kjp002@ucsd.edu>
2024-08-17 23:45:36 -0700

    fix type

--- components/ui/InfiniteCards.tsx
+++ components/ui/InfiniteCards.tsx
@@ -13,7 +13,7 @@ export const InfiniteMovingCards = ({
   items: {
     quote: string;
     name: string;
-    title: string;
+    title?: string;
   }[];
   direction?: "left" | "right";
   speed?: "fast" | "normal" | "slow";
